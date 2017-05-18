137d8916772fc5f5435dac50ab834528e9bef892
cmdedj <cmdedjz@gmail.com>
2017-05-18 15:33:19 +0800

    test

--- manage.py
+++ manage.py
@@ -11,19 +11,19 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+	return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 
 @manager.command
 def test():
-    """Run the unit tests."""
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+	"""Run the unit tests."""
+	import unittest
+	tests = unittest.TestLoader().discover('tests')
+	unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == '__main__':
-    manager.run()
-    print('a')
+	manager.run()
+	print(123)
