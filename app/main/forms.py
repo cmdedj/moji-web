abc405a7c7bf41ffeeb3f990a51c159834c26fb6
cmdedj <cmdedjz@gmail.com>
2017-06-26 22:31:39 +0800

    miao

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,15 +1,42 @@
 from flask_wtf import Form
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Length, Email, Regexp, ValidationError
+
+from app.models import Role, User
 
 
 class NameForm(Form):
-	name = StringField('What is your name?', validators=[DataRequired()])
-	submit = SubmitField('Submit')
+	name = StringField('你的名字是什么？', validators=[DataRequired()])
+	submit = SubmitField('提交')
 
 
 class EditProfileForm(Form):
-	name = StringField('Real name', validators=[Length(0, 64)])
-	location = StringField('Location', validators=[Length(0, 64)])
-	about_me = TextAreaField('About me')
-	submit = SubmitField('Submit')
+	name = StringField('真实姓名', validators=[Length(0, 64)])
+	location = StringField('地点', validators=[Length(0, 64)])
+	about_me = TextAreaField('关于我')
+	submit = SubmitField('提交')
+
+
+class EditProfileAdminForm(Form):
+
+	email = StringField('电子邮件', validators=[DataRequired(), Length(1, 64), Email()])
+	username = StringField('用户名', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, '用户名应该只包含字母，数字，点或下划线')])
+	confirmed = BooleanField('已确认')
+	role = SelectField('角色', coerce=int)
+	name = StringField('真实姓名', validators=[Length(0, 64)])
+	location = StringField('地点', validators=[Length(0, 64)])
+	about_me = TextAreaField('关于我')
+	submit = SubmitField('提交')
+
+	def __init__(self, user, *args, **kwargs):
+		super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+		self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+		self.user = user
+
+	def validate_email(self, field):
+		if field.data != self.user.email and User.query.filter_by(email=field.data).first():
+			raise ValidationError('电子邮件已存在。')
+
+	def validate_username(self, field):
+		if field.data != self.user.username and User.query.filter_by(username=field.data).first():
+			raise ValidationError('用户名已存在。')
--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,11 @@
 from flask import render_template, session, redirect, url_for, current_app, abort, flash
 from flask_login import login_required, current_user
+from app.decorators import admin_required
 from .. import db
-from ..models import User
+from ..models import User, Role
 from ..email import send_email
 from . import main
-from .forms import NameForm, EditProfileForm
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm
 
 
 @main.route('/', methods=['GET', 'POST'])
@@ -48,3 +49,30 @@ def edit_profile():
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
+
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.name.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('此个人信息已被更新。')
+        return redirect(url_for('main.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
