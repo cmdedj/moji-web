9be7753006c6d8e05152e745df1039256d25df7c
cmdedj <cmdedjz@gmail.com>
2017-07-10 23:49:02 +0800

    12a

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
 	TESTING = True
-	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-tests.sqlite')
 
 
 class ProductionConfig(Config):
--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 from flask import url_for
 
 from app import create_app, db
-from app.models import User, Role, Permission, Post
+from app.models import User, Role, Permission, Post, Follow
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -14,7 +14,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-	return dict(app=app, db=db, User=User, Role=Role, Permission=Permission, Post=Post)
+	return dict(app=app, db=db, User=User, Role=Role, Permission=Permission, Post=Post, Follow=Follow)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
